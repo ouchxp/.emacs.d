3332c5e19293738ce37845769b1813a18748c67a
Nan <ouchxp.me@gmail.com>
2018-05-11 17:22:03 +1200

    optimise scrolling

--- config/nw-base/nw-emacs.el
+++ config/nw-base/nw-emacs.el
@@ -1,5 +1,9 @@
 (defconst nw-emacs-cache-directory
   (concat user-emacs-directory ".cache"))
 
+;; better scrolling
+(setq scroll-preserve-screen-position t)
+(setq scroll-margin 0)
+(setq scroll-conservatively 101)
 
 (provide 'nw-emacs)
