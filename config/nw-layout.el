559ee29e43d54fa9c9f0964b43c635698c9dc4fb
Nan Wu <ouchxp.me@gmail.com>
2020-02-26 12:04:52 +1300

    small adjustments

--- config/nw-layout.el
+++ config/nw-layout.el
@@ -8,8 +8,7 @@
   :straight t
   :config
   (progn
-    ;; Switch to a project and use its name as the tag of
-    ;; the window config
+    ;; Switch to a project and use its name as the tag of the window config
     (defun nw-eyebrowse-switch-project ()
       (interactive)
       (let ((project-name (counsel-projectile-switch-project)))
@@ -19,9 +18,7 @@
            ;;(shorten-directory project-name 32)
            (file-name-nondirectory (directory-file-name project-name))))))
 
-    ;; (eval-after-load "counsel-projectile"
-    ;;   '(define-key counsel-projectile-mode-map [remap projectile-switch-project] #'nw-eyebrowse-switch-project))
-
+    ;; customise the switch project behavior
     (setq eyebrowse-new-workspace 'nw-eyebrowse-switch-project)
     (setq eyebrowse-tagged-slot-format "%s [%t]")
     ;; I don't really use its default key map
