2a406c7a9dc61dd1ac95eacd9acb0347b92f795d
Nan Wu <ouchxp.me@gmail.com>
2020-02-26 17:18:05 +1300

    switch to dracula theme

--- config/nw-base.el
+++ config/nw-base.el
@@ -55,7 +55,7 @@
 
     ;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each theme
     ;; may have their own settings.
-    (load-theme 'doom-one t)
+    (load-theme 'doom-dracula t)
 
     ;; Enable flashing mode-line on errors
     ;;(doom-themes-visual-bell-config)
